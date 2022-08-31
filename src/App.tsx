580fd0e6edc52ee9661480957fa87a4494d95556
Pedro Clerici <pedro.clerici@tutamail.com>
2022-08-31 16:35:11 -0300

    Refactored component to use react-responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,49 @@
 import React from 'react';
-
-import Grid from './components/Grid';
-
+import { useMediaQuery } from 'react-responsive';
+// import cryptoInfoService from './services/cryptoInfoService';
 import './styles/globals/_index.scss';
 
-const App = () => (
-  <Grid>
-    <p>Hello, world!</p>
-  </Grid>
-);
+import Grid from './components/Grid';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import CryptoHeading from './components/CryptoHeading';
+import PriceCalculator from './components/PriceCalculator';
+import PriceChart from './components/PriceChart';
+import PopularCryptos from './components/PopularCryptos';
+import CryptoPriceData from './components/CryptoPriceData';
+
+const App = () => {
+  const isMobile = useMediaQuery({ query: '(max-width: 800px)' });
+
+  const defaultLayout = (
+    <Grid>
+      <Header />
+      <main>
+        <CryptoHeading />
+        <PriceChart />
+        <CryptoPriceData />
+      </main>
+      <aside>
+        <PriceCalculator />
+        <PopularCryptos />
+      </aside>
+      <Footer />
+    </Grid>
+  );
+
+  const mobileLayout = (
+    <Grid>
+      <Header />
+      <CryptoHeading />
+      <PriceCalculator />
+      <PriceChart />
+      <PopularCryptos />
+      <CryptoPriceData />
+      <Footer />
+    </Grid>
+  );
+
+  return isMobile ? mobileLayout : defaultLayout;
+};
 
 export default App;
