e5ba25dbdbf3d9e09b7ad1b977c4b4d8db57e50a
Pedro Clerici <pedro.clerici@tutamail.com>
2022-09-07 22:09:07 -0300

    Added vsCurrency

---
+++ src/currencies/vsCurrency.ts
@@ -0,0 +1,27 @@
+export type VsCurrency = {
+  id: string
+  symbol: string,
+};
+
+export const vsCurrencyList: VsCurrency[] = [
+  {
+    id: 'usd',
+    symbol: '$',
+  },
+  {
+    id: 'brl',
+    symbol: 'R$',
+  },
+  {
+    id: 'eur',
+    symbol: '€',
+  },
+  {
+    id: 'cad',
+    symbol: 'C$',
+  },
+  {
+    id: 'aud',
+    symbol: 'A$',
+  },
+];
