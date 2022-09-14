f93a33c744f92bfdd9a8d5f2dae26b02ef79b740
Pedro Clerici <pedro.clerici@tutamail.com>
2022-09-14 15:49:05 -0300

    Added a toolbar for PriceChart

--- src/components/PriceChart/index.tsx
+++ src/components/PriceChart/index.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UserContext } from '../../context/userContext';
 import LineChart from './LineChart';
+import PrevTimeSelector from './PrevTimeSelector';
 import './styles.scss';
 
-const PriceChart = () => (
-  <LineChart />
-);
+const PriceChart = () => {
+  const { vsCurrency } = useContext(UserContext);
+
+  return (
+    <div className="price-chart">
+      <div className="price-chart__toolbar">
+        <PrevTimeSelector />
+        <span className="price-chart__vs-currency">
+          {vsCurrency.id.toUpperCase()}
+        </span>
+      </div>
+      <LineChart />
+    </div>
+  );
+};
 
 export default PriceChart;
