b399431af1654a8af341e8b588fb41adc09f0244
Pedro Clerici <pedro.clerici@tutamail.com>
2022-09-23 14:44:25 -0300

    Added more content to footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -7,7 +7,13 @@ interface Props {
 
 const Footer: React.FC<Props> = () => (
   <footer className="footer">
-    Footer
+    <p className="footer__description">
+      {'Coded & Designed by '}
+      <a href="https://www.linkedin.com/in/pedro-clerici-a810a123b" target="_blank" rel="noreferrer">Pedro Clerici</a>
+      <br />
+      {'Icons by '}
+      <a href="https://cryptoicons.co/" target="_blank" rel="noreferrer">CryptoIcons</a>
+    </p>
   </footer>
 );
 
