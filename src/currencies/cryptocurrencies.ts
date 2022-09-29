a41ef007cebfbcb049c5a87e0f3ab9d6c84e560f
Pedro Clerici <pedro.clerici@tutamail.com>
2022-09-29 20:42:38 -0300

    Added more cryptocurrencies

--- src/currencies/cryptocurrencies.ts
+++ src/currencies/cryptocurrencies.ts
@@ -81,6 +81,11 @@ export const cryptocurrenciesList: Cryptocurrency[] = [
     name: 'Avalanche',
   },
   {
+    id: 'tron',
+    symbol: 'trx',
+    name: 'Tron',
+  },
+  {
     id: 'wrapped-bitcoin',
     symbol: 'wbtc',
     name: 'Wrapped Bitcoin',
@@ -109,6 +114,191 @@ export const cryptocurrenciesList: Cryptocurrency[] = [
     id: 'chainlink',
     symbol: 'link',
     name: 'Chainlink',
+  },
+  {
+    id: 'polkadot',
+    symbol: 'dot',
+    name: 'Polkadot',
+  },
+  {
+    id: 'oxbitcoin',
+    symbol: '0xbtc',
+    name: '0xBitcoin',
+  },
+  {
+    id: '1inch',
+    symbol: '1inch',
+    name: '1inch',
+  },
+  {
+    id: 'arcblock',
+    symbol: 'abt',
+    name: 'Arcblock',
+  },
+  {
+    id: 'achain',
+    symbol: 'act',
+    name: 'Achain',
+  },
+  {
+    id: 'aeternity',
+    symbol: 'ae',
+    name: 'Aeternity',
+  },
+  {
+    id: 'algorand',
+    symbol: 'algo',
+    name: 'Algorand',
+  },
+  {
+    id: 'bitcoin-cash',
+    symbol: 'bch',
+    name: 'Bitcoin Cash',
+  },
+  {
+    id: 'quant-network',
+    symbol: 'qnt',
+    name: 'Quant',
+  },
+  {
+    id: 'apecoin',
+    symbol: 'ape',
+    name: 'ApeCoin',
+  },
+  {
+    id: 'vechain',
+    symbol: 'vet',
+    name: 'VeChain',
+  },
+  {
+    id: 'internet-computer',
+    symbol: 'icp',
+    name: 'Internet Computer',
+  },
+  {
+    id: 'tezos',
+    symbol: 'xtz',
+    name: 'tezos',
+  },
+  {
+    id: 'decentraland',
+    symbol: 'mana',
+    name: 'Decentraland',
+  },
+  {
+    id: 'chiliz',
+    symbol: 'chz',
+    name: 'Chiliz',
+  },
+  {
+    id: 'the-sandbox',
+    symbol: 'sand',
+    name: 'The Sandbox',
+  },
+  {
+    id: 'eos',
+    symbol: 'eos',
+    name: 'EOS',
+  },
+  {
+    id: 'theta-token',
+    symbol: 'theta',
+    name: 'Theta Network',
+  },
+  {
+    id: 'aave',
+    symbol: 'aave',
+    name: 'Aave',
+  },
+  {
+    id: 'true-usd',
+    symbol: 'tusd',
+    name: 'TrueUSD',
+  },
+  {
+    id: 'bitcoin-cash-sv',
+    symbol: 'bsv',
+    name: 'Bitcoin SV',
+  },
+  {
+    id: 'kucoin-shares',
+    symbol: 'kcs',
+    name: 'KuCoin',
+  },
+  {
+    id: 'iota',
+    symbol: 'miota',
+    name: 'IOTA',
+  },
+  {
+    id: 'bittorrent',
+    symbol: 'btt',
+    name: 'BitTorrent',
+  },
+  {
+    id: 'the-graph',
+    symbol: 'grt',
+    name: 'The Graph',
+  },
+  {
+    id: 'zcash',
+    symbol: 'zec',
+    name: 'Zcash',
+  },
+  {
+    id: 'huobi-token',
+    symbol: 'ht',
+    name: 'Huobi',
+  },
+  {
+    id: 'maker',
+    symbol: 'mkr',
+    name: 'Maker',
+  },
+  {
+    id: 'neo',
+    symbol: 'neo',
+    name: 'NEO',
+  },
+  {
+    id: 'havven',
+    symbol: 'snx',
+    name: 'Synthetix Network',
+  },
+  {
+    id: 'nexo',
+    symbol: 'nexo',
+    name: 'NEXO',
+  },
+  {
+    id: 'pax-gold',
+    symbol: 'paxg',
+    name: 'PAX Gold',
+  },
+  {
+    id: 'zilliqa',
+    symbol: 'zil',
+    name: 'Zilliqa',
+  },
+  {
+    id: 'enjincoin',
+    symbol: 'enj',
+    name: 'Enjin Coin',
+  },
+  {
+    id: 'basic-attention-token',
+    symbol: 'bat',
+    name: 'Basic Attention',
+  },
+  {
+    id: 'blockstack',
+    symbol: 'stx',
+    name: 'Stacks',
+  },
+  {
+    id: 'ravencoin',
+    symbol: 'rvn',
+    name: 'Ravencoin',
   },
 ];
 
