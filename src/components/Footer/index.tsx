52d10c1a94873d9f69791f9f8bbf48af6ff354db
Pedro Clerici <pedro.clerici@tutamail.com>
2022-10-21 23:04:18 -0300

    Added coingecko link to Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
-
+import coinGecko from '../../assets/coingecko.svg';
 import './styles.scss';
 
-interface Props {
-}
-
-const Footer: React.FC<Props> = () => (
+const Footer = () => (
   <footer className="footer">
     <p className="footer__description">
       {'Coded & Designed by '}
@@ -14,6 +11,12 @@ const Footer: React.FC<Props> = () => (
       {'Icons by '}
       <a href="https://cryptoicons.co/" target="_blank" rel="noreferrer">CryptoIcons</a>
     </p>
+    <div className="footer__powered-by">
+      <p>Powered by</p>
+      <a href="https://www.coingecko.com" target="_blank" rel="noreferrer">
+        <img className="footer_coingecko-logo" src={coinGecko} alt="ok-hand" />
+      </a>
+    </div>
   </footer>
 );
 
